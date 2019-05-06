922902d9fd4e66102edc0dd50c5fbe242c7a1cf7
Daniel Cobbinah <dmcobbinah@gmail.com>
2019-05-06 17:36:48 +0100

    jquery added and tweaked

--- js/main.js
+++ js/main.js
@@ -3,19 +3,23 @@ $(document).ready(function(){
     alert('The Submit button was pressed');
   });
 
-  $(".button--google a").click(function(){
+  $("#googlelink").click(function(){
     alert('The GOOGLE Link was pressed');
   });
 
-  $(".button--facebook a").click(function(){
+  $("#facebooklink").click(function(){
     alert('The FACEBOOK Link was pressed');
   });
 
-  $(".content--textbutton-black").click(function(){
+  $("#backbutton").click(function(){
     alert('The Back button was pressed');
   });
 
-  $(".content--textbutton").click(function(){
+  $("#forgotpassword").first().click(function(){
+    alert('The FORGOT PASSWORD button was pressed');
+  });
+
+  $("#signup").last().click(function(){
     alert('The Sign Up! button at the bottom of the page was pressed');
   });
 });
